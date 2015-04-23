4711a9cc38d5ef0545a1b1b53b2cfe3aef05b61d
foran <forestanalytic@hughes.net>
2015-04-23 12:36:09 -0500

    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse
+##
+## cacheSolve computes the inverse of the special "matrix", first checking to see
+## if an inverse has already been computed and cached.  If the inverse has been cached,
+## it retrieves the inverse from the cache.  If the inverse has not been cached, it 
+## calculates the inverse.  The inverse is returned.
+##
+##  Example use:
+##
+##  hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+##  h8 <- hilbert(8)
+##  m <- makeCacheMatrix(h8)
+##  cacheSolve(m) # this will calculate and cache the inverse
+##  cacheSolve(m) # this will retrieve the inverse from the cache
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function()x
+    setinv <- function(solve) m <<- solve
+    getinv <- function()m
+    list (set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)){
+        message ("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...) 
+    x$setinv(m)
+    m
 }
